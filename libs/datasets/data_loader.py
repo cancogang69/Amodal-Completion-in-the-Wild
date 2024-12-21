d2e7d240d781a42bd49dbf05d566503ff8855d33
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-21 12:56:11 +0700

    debug

--- libs/datasets/data_loader.py
+++ libs/datasets/data_loader.py
@@ -65,16 +65,16 @@ class DatasetLoader(object):
             org_src_ft = feat.permute(1, 2, 0).float().numpy()  # h x w x L
             org_src_ft_dict[layer_i] = org_src_ft
 
-        # return org_src_ft_dict
-
-        org_src_ft_dict = {}
-        for layer_i in [0, 1, 2, 3]:
-            feat_dir = feature_dirs + str(layer_i)
-            feat = torch.load(os.path.join(feat_dir, image_name[:-4] + ".pt"))
-            org_src_ft = feat.permute(1, 2, 0).float().numpy()  # h x w x L
-            org_src_ft_dict[layer_i] = org_src_ft
-
         return org_src_ft_dict
+
+        # org_src_ft_dict = {}
+        # for layer_i in [0, 1, 2, 3]:
+        #     feat_dir = feature_dirs + str(layer_i)
+        #     feat = torch.load(os.path.join(feat_dir, image_name[:-4] + ".pt"))
+        #     org_src_ft = feat.permute(1, 2, 0).float().numpy()  # h x w x L
+        #     org_src_ft_dict[layer_i] = org_src_ft
+
+        # return org_src_ft_dict
 
     def __combime_mask_with_sd_features(
         self, image_height, image_width, bbox, sd_features
